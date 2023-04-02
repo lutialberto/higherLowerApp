c52b045e122497073623b5d9031ed0070ab57751
lutialberto <betomdq.93@gmail.com>
2023-04-02 19:54:02 -0300

    useColors hook (#5)
    Co-authored-by: Alberto Luti <alberto_luti@edsa.com.ar>

---
+++ src/hooks/colors/Constants.ts
@@ -0,0 +1,12 @@
+import {BaseColors} from './Models';
+
+export const BASE_COLORS: BaseColors = {
+  black: '#222222',
+  white: '#FFFFFF',
+  red: '#F5333F',
+  gray: '#969696',
+  blue: '#0186FF',
+  green: '#1FDE6B',
+  orange: '#FF881A',
+  yellow: '#FFCD00',
+};
---
+++ src/hooks/colors/Models.ts
@@ -0,0 +1,17 @@
+export interface BaseColors {
+  white: string;
+  black: string;
+  red: string;
+  gray: string;
+  blue: string;
+  orange: string;
+  green: string;
+  yellow: string;
+}
+
+export interface Colors extends BaseColors {
+  background: string;
+  font: string;
+  primary: string;
+  disabled: string;
+}
